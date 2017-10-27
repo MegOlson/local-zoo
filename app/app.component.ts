54bf083a524c9d215e489ad97c8309a30f7546b6
mo <meganannetteolson@yahoo.com>
2017-10-27 08:26:58 -0700

    Add masterAnimalList with three complete zoo objects to root component.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,46 @@
 import { Component } from '@angular/core';
+import { Zoo } from './zoo.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app/app.component.html'
 })
 
-export class AppComponent { }
+export class AppComponent {
+  selectedAnimal = null;
+  masterAnimalList: Zoo[] = [
+    new Zoo(
+      "Arctic Fox",
+      "Moon",
+      2,
+      "Carnivore",
+      "Northern Trail",
+      5,
+      "Female",
+       "Cool shade",
+      "Loud noises"
+    ),
+    new Zoo(
+      "Ocelot",
+      "Prince",
+      4,
+      "Carnivore",
+      "Tropical Rain Forest Building",
+      6,
+      "Male",
+      "Laying in the sunshine",
+      "Toys that are not rope-based"
+    ),
+    new Zoo(
+      "Northwest Black Tailed Deer",
+      "Tinkerbell",
+      8,
+      "Herbivore",
+      "Northern Trail",
+      2,
+      "Female",
+      "Delicate roots and leaves",
+      "Loud Noises"
+    )
+  ]
+}
